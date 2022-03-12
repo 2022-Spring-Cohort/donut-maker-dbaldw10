4119b35d3f44e2d54c122b2ba388d016d6607c99
Dallas Baldwin <Dbaldw10@kent.edu>
2022-03-11 23:48:10 -0500

    donut it1

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const container = (document.querySelector('.container').innerText =
   'mmmmmmmmm doughnuts.... ahhhhhhh!');
+
+  var clicks = 0;
+  function increment(){
+      clicks = clicks + 1;
+      counter.innerText = clicks;
+  }
+  function reset(){
+    clicks = 0;
+    counter.innerText = clicks;
+  }
+  
